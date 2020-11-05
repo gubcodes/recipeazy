e55231fcf06b906db1c277b587c63db80b12bee7
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2020-11-05 11:16:13 -0500

    final edits?

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -74,7 +74,7 @@ const RecipeNavbar = (props) => {
     <div className={classes.root}>
 
       <Navbar color="light" light expand="md" fixed="top">
-        <NavbarBrand href="/home"><span className={classes.colorText2}>Recip</span><span className={classes.colorText}>Eazy</span></NavbarBrand>
+        <NavbarBrand href="/"><span className={classes.colorText2}>Recip</span><span className={classes.colorText}>Eazy</span></NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
@@ -91,7 +91,7 @@ const RecipeNavbar = (props) => {
           <Nav>
             <Auth updateToken={updateToken}/>
           </Nav>
-          <NavLink href="/home">
+          <NavLink href="/">
             {localStorage.getItem('token') !== null ? <Button className={classes.button1} id="buttonHover"
             style={{ marginRight: 25,
             backgroundColor: '#E717E8',
@@ -106,7 +106,7 @@ const RecipeNavbar = (props) => {
         </Collapse>
       </Navbar>
       <Switch>
-            <Route exact path="/home"><Home/></Route>
+            <Route exact path="/"><Home/></Route>
             <Route exact path="/search"><Search/></Route>
             <Route exact path="/list">{!isAuth ? <Redirect to='/'/> : <ListDisplay/>}</Route>
       </Switch>
